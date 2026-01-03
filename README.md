6117fb3b2004c4670ce68369ae794f1297cb0af0
tlanders <tlanders@landersconsulting.biz>
2026-01-03 16:28:52 -0600

    basic oauth working

---
+++ README.md
@@ -0,0 +1,6 @@
+Oauth2 Simple Demo
+
+Uses authorization code flow to request a token from Auth0 using a client ID and secret on the
+server-side. Must specify issuer URI, credentials, and scopes in application.properties.
+
+Application must also be setup on Auth0 as a Regular Web Application prior to authenticating.
---
+++ src/main/java/biz/lci/demo/oauth2simpledemo/Oauth2SimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package biz.lci.demo.oauth2simpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SimpleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2SimpleDemoApplication.class, args);
+    }
+
+}
